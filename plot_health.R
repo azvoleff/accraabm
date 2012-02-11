a55c648c9423e3115d4b31b185a2cbd2a613a96b
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2012-02-11 11:55:30 -0800

    Preliminary bugfixing on health plotting code.

--- plot_health.R
+++ plot_health.R
@@ -26,6 +26,7 @@
 
 require(animation)
 require(raster)
+require(gstat)
 
 PLOT_WIDTH = 8.33
 PLOT_HEIGHT = 5.53
@@ -34,16 +35,16 @@ PLOT_WIDTH_PIXELS = round(PLOT_WIDTH * DPI)
 PLOT_HEIGHT_PIXELS = round(PLOT_HEIGHT * DPI)
 
 DATA_PATH <- commandArgs(trailingOnly=TRUE)[1]
-DATA_PATH <- "R:/Data/AccraABM/Runs/Default/20120210-232115_azvoleff-THINK"
+DATA_PATH <- "R:/Data/AccraABM/Runs/Default/20120211-114145_azvoleff-THINK"
 
 timesteps <- read.csv(paste(DATA_PATH, "/time.csv", sep=""))
     
 plot_health<- function(DATA_PATH, timestep) {
     # Load the grid on which to Krige. This GeoTIFF also will be used to mask 
     # the final kriging results.
-    kriglocations <- raster(paste(DATA_PATH, "AccraABM_world_mask.tif", sep="/"))
-    kriglocations[kriglocations==minValue(kriglocations)] <- 0
-    kriglocations[kriglocations==maxValue(kriglocations)] <- 1
+    kriglocations <- readGDAL(paste(DATA_PATH, "AccraABM_world_mask.tif", sep="/"))
+    kriglocations$band1[kriglocations$band1==min(kriglocations$band1)] <- 0
+    kriglocations$band1[kriglocations$band1==max(kriglocations$band1)] <- 1
 
     persons <- read.csv(paste(DATA_PATH, "/psns_time_", timestep, ".csv", sep=""))
     persons.spatial <- SpatialPointsDataFrame(cbind(persons$x_utm30, persons$y_utm30), persons,
@@ -54,7 +55,6 @@ plot_health<- function(DATA_PATH, timestep) {
     #v.fit <- fit.variogram(v, vgm(1, "Exp", 6000, .05))
     v.fit <- fit.variogram(v, vgm(1, "Sph", 6000, .05))
     krigged.ord <- krige(health~1, persons.spatial, kriglocations, v.fit)
-
     krigged.ord.pred <- krigged.ord["var1.pred"]
     # Mask out areas outside Accra using the study area mask. Set areas outside 
     # study area to -999
@@ -62,9 +62,10 @@ plot_health<- function(DATA_PATH, timestep) {
     krigged.ord.pred$var1.pred[krigged.ord.pred$var1.pred==0] <- -1
     proj4string(krigged.ord.pred) <- CRS(proj4string(kriglocations))
     krigged_imagefile <- paste(DATA_PATH, "/health_ordinary_krig_END.tif", sep="")
-    image(krigged.ord.pred, 
-          main=paste("Timestep:", format(timestep, width=2)), axes=FALSE, 
-          xlab="", ylab="", cex.main=1.8, cex.sub=1.8, sub=paste("Vegetation: ", veg_pct, "%, Non-vegetation: ", nonveg_pct, "%", sep=""))
+    image(krigged.ord.pred, main=paste("Timestep:", format(timestep, width=2)), 
+          axes=FALSE, xlab="", ylab="", cex.main=1.8, cex.sub=1.8, 
+          sub=paste("Vegetation: ", veg_pct, "%, Non-vegetation: ", nonveg_pct, 
+                    "%", sep=""))
 }
 
 ani.options(ffmpeg=shQuote('C:/Program Files (x86)/ffmpeg-git-6833fe4-win32-static/bin/ffmpeg.exe'))
